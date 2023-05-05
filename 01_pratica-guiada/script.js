2586a34ef0f27e2a0880f9468c0c1164c43a3985
sivilela <sivilela.s@gmail.com>
2023-05-04 21:15:33 -0300

    Realizado exercício de fixação

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,44 @@
 //CODIGO AQUI
+
+// let saldo = 15.00
+// saldo += 22.40
+// saldo -= 12.34
+// saldo /= 2
+// saldo *= 3.4
+// //resto da divisão por 3 = 0.60 centavos
+// //módulo
+// saldo %= 3
+// console.log(saldo);
+
+//próximo
+//let operacao = 2+2*2
+//let operacao = 2*(2+2)
+//console.log(operacao);
+
+//próximo
+
+// const comparacao = 5 === 5
+// console.log(comparacao);
+// const comparacao2 = 5!==5
+// console.log(comparacao2);
+// const comparacao3 = 5!== "5"
+// console.log(comparacao3);
+// const comparacao4 = "5" === "cinco"
+// console.log(comparacao4);
+// const comparacao5 = typeof 5 === typeof 20
+// console.log(comparacao5);
+
+//próximo
+// const primeiroNumero = 57
+// const segundoNumero = 13
+// verificacao = primeiroNumero === segundoNumero
+// console.log(verificacao);
+// verificacao = primeiroNumero <= segundoNumero
+// console.log(verificacao);
+// verificacao = primeiroNumero > segundoNumero
+// console.log(verificacao);
+// verificacao = primeiroNumero < segundoNumero
+// console.log(verificacao);
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,15 @@
-// EXERCICIO DE FIXAÇÃO
+EXERCICIO DE FIXAÇÃO
+
+
+const minhaIdade = Number(prompt("Minha idade é: "))
+console.log(minhaIdade); 
+const amigaIdade = Number(prompt("Idade da minha amiga: "))
+console.log(amigaIdade);
+comparacao = minhaIdade > amigaIdade
+console.log(comparacao)
+diferenca = minhaIdade - amigaIdade
+console.log(diferenca);
+
+console.log("Eu tenho",minhaIdade, " anos e você tem",amigaIdade, 
+"anos, sou mais nova do que você",diferenca, "anos de idade.");
 
